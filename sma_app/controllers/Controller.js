eea0f09abe29822579e24059e7eb6fcd6b745d9a
thejatmik <jatmikatejasukmana@gmail.com>
2020-02-26 18:14:12 +0700

    resolve merge conflict

--- sma_app/controllers/Controller.js
+++ sma_app/controllers/Controller.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 const { Owner } = require('../models/index');
 
-=======
->>>>>>> 62c26435ca2058d0c584c7929a2303a54532b3eb
 class Controller {
     static getRoot(req, res) {
         res.send("hewo root controll owo");
@@ -11,7 +8,6 @@ class Controller {
         res.render("login");
     }
     static postLogin(req, res) {
-<<<<<<< HEAD
         Owner.findAll()
         .then(result => {
             res.send(result);
@@ -20,9 +16,6 @@ class Controller {
             res.send(err);
         })
         // res.send(req.body);
-=======
-        res.send(req.body);
->>>>>>> 62c26435ca2058d0c584c7929a2303a54532b3eb
     }
 }
 
