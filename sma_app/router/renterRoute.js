dc658d1413f8d5c1647d74ee1e3a5112291c7eac
ryan <ryanmaulanaputra@gmail.com>
2020-02-26 16:50:11 +0700

    add middleware skeleton and owner & renter route

---
+++ sma_app/router/renterRoute.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/', RenterController.showList);
+router.get('/booking/', RenterController.showBookForm); // milih mobil, milih startdate & days
+router.post('/booking/', RenterController.book);
